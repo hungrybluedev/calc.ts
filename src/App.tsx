31c78fe3847bbf7a232c0b711cdebbe4320d8b68
Subhomoy Haldar <hungrybluedev@gmail.com>
2022-07-04 02:42:47 +0530

    App: refactor, move out logic and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,73 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
 import "./App.css";
 
+import DigitButton from "./DigitButton";
+import OperationButton from "./OperationButton";
+
+import calculatorReducer from "./logic";
+import { ActionType } from "./definitions";
+
 function App() {
+  const [{ currentOperand, previousOperand, opSymbol, operation }, dispatch] =
+    useReducer(calculatorReducer, {
+      currentOperand: "0",
+      previousOperand: "",
+      opSymbol: "",
+    });
+
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">123+</div>
-        <div className="current-operand">456</div>
+        <div className="previous-operand">
+          {previousOperand} {opSymbol}
+        </div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
-      <button className="span-two">AC</button>
-      <button>DEL</button>
-      <button>&#247;</button>
-      <button>1</button>
-      <button>2</button>
-      <button>1</button>
-      <button>&#215;</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>-</button>
-      <button>.</button>
-      <button>0</button>
-      <button className="span-two">=</button>
+      <button
+        className="span-two"
+        onClick={() =>
+          dispatch({
+            type: ActionType.clear,
+          })
+        }
+      >
+        AC
+      </button>
+      <button
+        onClick={() =>
+          dispatch({
+            type: ActionType.delete_digit,
+          })
+        }
+      >
+        DEL
+      </button>
+      <OperationButton operation="/" symbol="&#247;" dispatch={dispatch} />
+      <DigitButton dispatch={dispatch} digit="1" />
+      <DigitButton dispatch={dispatch} digit="2" />
+      <DigitButton dispatch={dispatch} digit="3" />
+      <OperationButton operation="*" symbol="&#215;" dispatch={dispatch} />
+      <DigitButton dispatch={dispatch} digit="4" />
+      <DigitButton dispatch={dispatch} digit="5" />
+      <DigitButton dispatch={dispatch} digit="6" />
+      <OperationButton operation="+" dispatch={dispatch} />
+      <DigitButton dispatch={dispatch} digit="7" />
+      <DigitButton dispatch={dispatch} digit="8" />
+      <DigitButton dispatch={dispatch} digit="9" />
+      <OperationButton operation="-" dispatch={dispatch} />
+      <DigitButton dispatch={dispatch} digit="." />
+      <DigitButton dispatch={dispatch} digit="0" />
+      <button
+        className="span-two"
+        onClick={() =>
+          dispatch({
+            type: ActionType.evaluate,
+          })
+        }
+      >
+        =
+      </button>
     </div>
   );
 }
