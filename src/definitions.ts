45a0496b5aaeae3809a22262105c3db0b9a071c7
Subhomoy Haldar <hungrybluedev@gmail.com>
2022-07-04 02:35:11 +0530

    definitions: refactor out types

---
+++ src/definitions.ts
@@ -0,0 +1,37 @@
+export enum ArithmeticOperation {
+  addition,
+  subtraction,
+  multiplication,
+  division,
+}
+
+export const OP_MAP: Map<string, ArithmeticOperation> = new Map([
+  ["+", ArithmeticOperation.addition],
+  ["-", ArithmeticOperation.subtraction],
+  ["*", ArithmeticOperation.multiplication],
+  ["/", ArithmeticOperation.division],
+]);
+
+export interface AppState {
+  currentOperand: string;
+  previousOperand: string;
+  opSymbol?: string;
+  operation?: ArithmeticOperation;
+}
+
+export enum ActionType {
+  add_digit,
+  choose_operation,
+  clear,
+  delete_digit,
+  evaluate,
+}
+
+export interface AppAction {
+  type: ActionType;
+  payload?: {
+    digit: string;
+    operation: string;
+    symbol: string;
+  };
+}
