01884e4c35f4ed93baa6ef6078f74e52f0485d2d
Subhomoy Haldar <hungrybluedev@gmail.com>
2022-07-04 23:19:30 +0530

    config: extract icons array

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,49 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
+const icons = [
+  {
+    src: "images/icons/icon-72x72.png",
+    sizes: "72x72",
+    type: "image/png",
+  },
+  {
+    src: "images/icons/icon-96x96.png",
+    sizes: "96x96",
+    type: "image/png",
+  },
+  {
+    src: "images/icons/icon-128x128.png",
+    sizes: "128x128",
+    type: "image/png",
+  },
+  {
+    src: "images/icons/icon-144x144.png",
+    sizes: "144x144",
+    type: "image/png",
+  },
+  {
+    src: "images/icons/icon-152x152.png",
+    sizes: "152x152",
+    type: "image/png",
+  },
+  {
+    src: "images/icons/icon-192x192.png",
+    sizes: "192x192",
+    type: "image/png",
+  },
+  {
+    src: "images/icons/icon-384x384.png",
+    sizes: "384x384",
+    type: "image/png",
+  },
+  {
+    src: "images/icons/icon-512x512.png",
+    sizes: "512x512",
+    type: "image/png",
+  },
+];
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -22,48 +65,7 @@ export default defineConfig({
         orientation: "portrait",
         scope: "/",
         start_url: "/",
-        icons: [
-          {
-            src: "images/icons/icon-72x72.png",
-            sizes: "72x72",
-            type: "image/png",
-          },
-          {
-            src: "images/icons/icon-96x96.png",
-            sizes: "96x96",
-            type: "image/png",
-          },
-          {
-            src: "images/icons/icon-128x128.png",
-            sizes: "128x128",
-            type: "image/png",
-          },
-          {
-            src: "images/icons/icon-144x144.png",
-            sizes: "144x144",
-            type: "image/png",
-          },
-          {
-            src: "images/icons/icon-152x152.png",
-            sizes: "152x152",
-            type: "image/png",
-          },
-          {
-            src: "images/icons/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "images/icons/icon-384x384.png",
-            sizes: "384x384",
-            type: "image/png",
-          },
-          {
-            src: "images/icons/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
+        icons: icons,
         screenshots: [
           {
             src: "images/screenshot.png",
@@ -74,48 +76,7 @@ export default defineConfig({
         shortcuts: [
           {
             name: "Calculator",
-            icons: [
-              {
-                src: "images/icons/icon-72x72.png",
-                sizes: "72x72",
-                type: "image/png",
-              },
-              {
-                src: "images/icons/icon-96x96.png",
-                sizes: "96x96",
-                type: "image/png",
-              },
-              {
-                src: "images/icons/icon-128x128.png",
-                sizes: "128x128",
-                type: "image/png",
-              },
-              {
-                src: "images/icons/icon-144x144.png",
-                sizes: "144x144",
-                type: "image/png",
-              },
-              {
-                src: "images/icons/icon-152x152.png",
-                sizes: "152x152",
-                type: "image/png",
-              },
-              {
-                src: "images/icons/icon-192x192.png",
-                sizes: "192x192",
-                type: "image/png",
-              },
-              {
-                src: "images/icons/icon-384x384.png",
-                sizes: "384x384",
-                type: "image/png",
-              },
-              {
-                src: "images/icons/icon-512x512.png",
-                sizes: "512x512",
-                type: "image/png",
-              },
-            ],
+            icons: icons,
             short_name: "Calculator",
             description: "Open the calculator",
             url: "/",
