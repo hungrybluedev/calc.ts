00e93604c773b9a49f44aed0e56d148494eedfe0
Subhomoy Haldar <hungrybluedev@gmail.com>
2022-07-09 21:39:34 +0530

    config: simplify vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    includeSource: ["src/**/*.ts"],
     coverage: {
       reporter: ["text", "html-spa", "lcov"],
     },
